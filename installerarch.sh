3d5f1b779cec79284530b7e96433fcd748ff15b5
tdisanayaka <96068260+tdisanayaka@users.noreply.github.com>
2024-12-15 22:15:02 +0530

    Update installerarch.sh

--- installerarch.sh
+++ installerarch.sh
@@ -1,5 +1,5 @@
 pacman -Syu
 pacman -S python python-pip
 pip install -r requirements.txt  --break-system-packages
-mv bugfinder.py /usr/bin/bughfinder
+mv bugfinders.py /usr/bin/bughfinder
 chmod +x /usr/bin/bughfinder
