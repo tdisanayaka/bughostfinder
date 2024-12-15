3520cee6dd8e0c5daf9e7428076fb1c2813b45ab
tdisanayaka <96068260+tdisanayaka@users.noreply.github.com>
2024-12-15 21:20:31 +0530

    Update installertermux.sh

--- installertermux.sh
+++ installertermux.sh
@@ -2,4 +2,4 @@ apt update
 apt upgrade -y
 apt install python python-pip -y
 pip install -r requirements.txt
-mv bugfinder.py /data/data/com.termux/files/usr/bin/bugfinder
+mv bugfinder.py /data/data/com.termux/files/usr/bin/bughfinder
